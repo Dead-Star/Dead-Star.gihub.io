05c632fcc59fed9c5b620235fc16358f0f532bc6
Dead-Star <youmirinbro3@gmail.com>
2017-11-26 21:00:48 +0400

    main

---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	
+
+
+
+	$(".button_menu").click(function() {
+		$("#sandwich").toggleClass("active");
+		
+		if ($(".top_menu").is(":visible"))
+		{
+			$(".top_menu").fadeOut(600);
+
+		}
+		
+		else {
+		$(".top_menu").fadeIn(600);
+		$(".top_menu_li .top_menu_a").toggleClass("animated rollIn")
+		};
+	});	
+
+	$(".top_menu_li  .top_menu_a").click(function() {
+	$(".top_menu").fadeOut(600)
+	$("#sandwich").toggleClass("active");
+
+	});
+	
+
+
+	
+	
+
+});
+
